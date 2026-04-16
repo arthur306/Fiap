51878a7eefb840cbfd2d95c055e436118a6f1f8e
Arthur Michelini <arthurmichelini308@gmail.com>
2026-04-16 11:08:53 -0300

    Atividade cap 6

--- Atividade_6.py
+++ Atividade_6.py
@@ -1,3 +1,56 @@
+# Importando bibliotecas
+import oracledb
+import pandas as pd
+import json
+
+# Dados de conexão com banco de dados
+usuario = "USER"
+senha = "SENHA"
+dsn = "oracle.fiap.com.br:1521/ORCL"
+
+# Função para estabelecer conexão
+def conectar():
+    try:
+        conexao = oracledb.connect(
+        user='USUÁRIO',
+        password="SENHA",
+        dsn='oracle.fiap.com.br:1521/ORCL'
+    )
+        return conexao
+    except Exception as e:
+        print(f"Erro ao conectar ao Oracle: {e}")
+        return None
+    
+# Criando tabela no banco de dados
+def criar_tabela(conexao):
+    try:
+        cursor = conexao.cursor()
+        sql = """
+        CREATE TABLE GS_ESTOQUE_AGRO (
+            id_produto NUMBER GENERATED AS IDENTITY PRIMARY KEY, 
+            nome_produto VARCHAR2(100) NOT NULL, 
+            quantidade NUMBER DEFAULT 0
+            )
+            """
+        cursor.execute(sql)
+        print("Tabela criada com sucesso!")
+    except Exception as e:
+        if "ORA-00955" in str(e):
+            print("A tabela ja existe.")
+        else:
+            print(f"Erro: {e}")
+
+# Salvando no banco de dados
+def salvar_no_banco(conexao, nome, qtd):
+    try:
+        cursor = conexao.cursor()
+        sql = "INSERT INTO GS_ESTOQUE_AGRO (nome_produto, quantidade) VALUES (:1, :2)"
+        cursor.execute(sql, (nome, qtd))
+        conexao.commit()
+        
+    except Exception as e:
+        print(f"Erro ao salvar: {e}")
+
 #---Orientações---
 
 #Logística: controle de estoque
@@ -9,39 +62,77 @@
 
 lista_produtos = list()
 lista_quantidade = list()
+
 #Funções subalgoritmo:
-def cadastrar():
-    nome = str(input("Digite o nome do produto:"))
-    quantidade = int(input("Quantidade do produto:"))
-    lista_produtos.append(nome)
-    lista_quantidade.append(quantidade)
-    print("Produtos cadastrados com sucesso.")
+def cadastrar(conexao):
+    nome = str(input("Digite o nome do produto: ")).lower()
+    quantidade = int(input("Quantidade do produto: "))
+
+    produto = (nome, quantidade)
+
+    lista_produtos.append(produto[0])
+    lista_quantidade.append(produto[1])
+
+    salvar_no_banco(conexao, nome, quantidade)
+
+    with open("estoque.json", "w") as f:
+        json.dump(list(zip(lista_produtos, lista_quantidade)), f, indent=4)
+
+    print(f"{nome} registrado no banco de dados!")
     
-def adicinar():
-    print(lista_produtos)
-    nome = str(input("Digite o nome do produto para adicionar:\n"))
+def adicionar():
+    if not lista_produtos:
+        print("O estoque está vazio.")
+        return
+    print(f"Produtos atuais {lista_produtos}")
+    nome = str(input("Digite o nome do produto para adicionar:\n")).lower()
     if nome in lista_produtos:
-        indice = lista_produtos.index(nome)
-        qtd = int(input("Quantidade a adicionar: "))
-        lista_quantidade[indice] += qtd
-        print("Quantidade adicionada com sucesso.")
+        indice = lista_produtos.index(nome) 
+        try:       
+            qtd = int(input("Quantidade a adicionar: "))
+            if qtd < 0:
+                print("Erro: Digite um valor valido.")
+            else:
+                lista_quantidade[indice] += qtd
+
+                with open("estoque.json", "w") as f:
+                    json.dump(list(zip(lista_produtos, lista_quantidade)), f, indent=4)
+
+                print(f"{nome} adicionado com sucesso. {lista_quantidade[indice]}")               
+        except ValueError:
+            print('Erro! Digite apenas números')
     else:
-        print("Produno inexistente.")
+        print("Produto inexistente.")
+
 def retirar():
-    print(lista_produtos)
-    nome = str(input("Digite o nome do produto para adicionar:\n"))
+    if not lista_produtos:
+        print("O estoque está vazio.")
+        return
+    print(f"Produtos disponiveis {lista_produtos}")
+    nome = str(input("Digite o nome do produto para retirar:\n")).lower()
     if nome in lista_produtos:
         indice = lista_produtos.index(nome)
-        qtd = int(input("Quantidade a retirar: "))
-        if lista_quantidade[indice] >= qtd:
-            lista_quantidade[indice] -= qtd
-            print("Quantidade retirada com sucesso.")
-        else:
-            print("Quantidade insuficiente.")
+        try:
+            qtd = int(input("Quantidade a retirar: "))
+            if qtd < 0:
+                print("Erro: Digite um valor valido.")
+            elif qtd > lista_quantidade[indice]:
+                print(f"Erro: Tem apenas {lista_quantidade[indice]} em estoque.")
+            else:           
+                lista_quantidade[indice] -= qtd
+
+                with open("estoque.json", "w") as f:
+                    json.dump(list(zip(lista_produtos, lista_quantidade)), f, indent=4)
+
+                print(f"Quantidade de {nome} retirada com sucesso.")
+        except ValueError:
+            print("Valor invalido.")        
     else:
         print("Produto não encontrado.")    
+
 def verificar():
     dicionario = dict(zip(lista_produtos, lista_quantidade))
+    lista_tuplas = list(zip(lista_produtos, lista_quantidade))
     while True:
           try:
               verificacao = int(input("\nEscolha o que quer fazer:\n"
@@ -54,16 +145,24 @@ def verificar():
                 case 1:
                     print("\n---Produtos---\n")
                     for produto, quantidade in dicionario.items():
-                        print(f"{produto}: {quantidade}")
+                        if quantidade <= 10:
+                            print(f"{produto}: {quantidade} ESTOQUE BAIXO")
+                        else:
+                            print(f"{produto}: {quantidade}")
+                    if quantidade <= 10:
+                        print("\n---ALERTA---\n")
+                        for item in lista_tuplas:
+                            if item[1] <= 10:
+                                print(f"{item} ESTOQUE BAIXO")
+
                 case 2:
                     break
                 
                 case _:
                     print("Opção inválida.")
-                    
 #Menu subalgoritmo:
 
-def menu():
+def menu(conexao):
     while True:
         try:
             escolha = int(input("\nSelecione uma das opções:\n"
@@ -77,9 +176,9 @@ def menu():
             continue
         match escolha:
             case 1:
-                cadastrar()
+                cadastrar(conexao)
             case 2:
-                adicinar()
+                adicionar()
             case 3:
                 retirar()
             case 4:
@@ -90,8 +189,17 @@ def menu():
             case _:
                 print("Opção inválida")
 
-                      
-import json
-#Menu
+
+# ---- Execução Principal ----
+
+# Testando a conexão 
+teste = conectar()
+if teste:
+    print("Conexão OK!")  
+
+# Chamando o criador de tabela
+criar_tabela(teste)                 
+
+# Inicia o menu
 print("===Bem-vindo ao gerenciador de estoques agrônomos===")
-menu()
+menu(teste)
